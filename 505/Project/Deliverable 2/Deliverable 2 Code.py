91c4db436791aa7f946ea5534eca72077444fd37
SeomanReborn <cplazony@gmail.com>
2025-09-19 12:56:21 -0400

    Update Deliverable 2 Code.py
    Finished Question 1

--- 505/Project/Deliverable 2/Deliverable 2 Code.py
+++ 505/Project/Deliverable 2/Deliverable 2 Code.py
@@ -18,6 +18,8 @@ from scipy.stats import weibull_min # For pweibull() equivalent (see line 462)
 exhaust = pd.read_excel(r'C:\Users\manam\Documents\GitHub\Purdue\505\Project\10_1_Class 8 Fleet Data.xlsx')
 fuelecon = pd.read_excel(r'C:\Users\manam\Documents\GitHub\Purdue\505\Project\10_2_Class 8 Fleet Fuel Economy.xlsx')
 #exhaust starts on row 1
+exhaust = exhaust.iloc[1:].reset_index(drop=True)
+
 
 #rename Truck Col
 exhaust.rename(columns={'Unnamed: 0': 'Truck'}, inplace=True)
@@ -26,6 +28,7 @@ exhaust.rename(columns={'Unnamed: 0': 'Truck'}, inplace=True)
 month = 'Month'
 mpg = 'Average Fuel Economy (mpg)'
 fuelTypeEcon = 'Fleet'
+season = 'Season'
 
 truck = 'Truck'
 sample = 'Samp. #'
@@ -63,3 +66,88 @@ plt.legend()
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+ulsdMpg.describe()
+b20Mpg.describe()
+
+month_to_season = {
+    'Dec': 'Winter', 'Jan': 'Winter', 'Feb': 'Winter',
+    'Mar': 'Spring', 'Apr': 'Spring', 'May': 'Spring',
+    'June': 'Summer', 'July': 'Summer', 'Aug': 'Summer',
+    'Sept': 'Fall', 'Oct': 'Fall', 'Nov': 'Fall'
+}
+
+ulsdMpg.loc[:,'Season'] = ulsdMpg[month].map(month_to_season)
+b20Mpg.loc[:,'Season'] = b20Mpg[month].map(month_to_season)
+
+# Spring
+ulsdMpgSpring = ulsdMpg.loc[ulsdMpg[season] == 'Spring']
+b20MpgSpring = b20Mpg.loc[b20Mpg[season] == 'Spring']
+
+# Summer
+ulsdMpgSummer = ulsdMpg.loc[ulsdMpg[season] == 'Summer']
+b20MpgSummer = b20Mpg.loc[b20Mpg[season] == 'Summer']
+
+# Fall
+ulsdMpgFall = ulsdMpg.loc[ulsdMpg[season] == 'Fall']
+b20MpgFall = b20Mpg.loc[b20Mpg[season] == 'Fall']
+
+# Winter
+ulsdMpgWinter = ulsdMpg.loc[ulsdMpg[season] == 'Winter']
+b20MpgWinter = b20Mpg.loc[b20Mpg[season] == 'Winter']
+
+ulsdMpg.loc[ulsdMpg[season] == 'Spring'].describe()
+b20Mpg.loc[b20Mpg[season] == 'Spring'].describe()
+
+ulsdMpg.loc[ulsdMpg[season] == 'Summer'].describe()
+b20Mpg.loc[b20Mpg[season] == 'Summer'].describe()
+
+ulsdMpg.loc[ulsdMpg[season] == 'Fall'].describe()
+b20Mpg.loc[b20Mpg[season] == 'Fall'].describe()
+
+ulsdMpg.loc[ulsdMpg[season] == 'Winter'].describe()
+b20Mpg.loc[b20Mpg[season] == 'Winter'].describe()
+
+plt.close()
+fig, axs = plt.subplots(2, 2, figsize=(8, 6))
+fig.suptitle('Seasonal Fuel Economy Comparison: ULSD vs B20', fontsize=16)
+
+
+# --- SPRING ---
+axs[0, 0].plot(ulsdMpgSpring[month], ulsdMpgSpring[mpg], label='ULSD', linestyle='-', marker='o', color='blue')
+axs[0, 0].plot(b20MpgSpring[month], b20MpgSpring[mpg], label='B20', linestyle='--', marker='s', color='red')
+axs[0, 0].set_title('Spring')
+axs[0, 0].set_ylabel('MPG')
+axs[0, 0].legend()
+axs[0, 0].grid(True)
+
+# --- SUMMER ---
+axs[0, 1].plot(ulsdMpgSummer[month], ulsdMpgSummer[mpg], label='ULSD', linestyle='-', marker='o', color='blue')
+axs[0, 1].plot(b20MpgSummer[month], b20MpgSummer[mpg], label='B20', linestyle='--', marker='s', color='red')
+axs[0, 1].set_title('Summer')
+axs[0, 1].legend()
+axs[0, 1].grid(True)
+
+# --- FALL ---
+axs[1, 0].plot(ulsdMpgFall[month], ulsdMpgFall[mpg], label='ULSD', linestyle='-', marker='o', color='blue')
+axs[1, 0].plot(b20MpgFall[month], b20MpgFall[mpg], label='B20', linestyle='--', marker='s', color='red')
+axs[1, 0].set_title('Fall')
+axs[1, 0].set_xlabel('Month')
+axs[1, 0].set_ylabel('MPG')
+axs[1, 0].legend()
+axs[1, 0].grid(True)
+
+# --- WINTER ---
+axs[1, 1].plot(ulsdMpgWinter[month], ulsdMpgWinter[mpg], label='ULSD', linestyle='-', marker='o', color='blue')
+axs[1, 1].plot(b20MpgWinter[month], b20MpgWinter[mpg], label='B20', linestyle='--', marker='s', color='red')
+axs[1, 1].set_title('Winter')
+axs[1, 1].set_xlabel('Month')
+axs[1, 1].legend()
+axs[1, 1].grid(True)
+
+# Adjust layout
+plt.tight_layout(rect=[0, 0, 1, 0.95])
+plt.show()
+
+
+
